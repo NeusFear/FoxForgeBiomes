648820de9c7b8b9a2b414f29b9572b7d777d0aec
SkyForge_ <goldr1000ftw@gmail.com>
2018-02-11 17:24:51 -0600

    Configured Dead Grass and updated textures

--- src/main/java/foxforgebiomes/blocks/BlockDeadGrass.java
+++ src/main/java/foxforgebiomes/blocks/BlockDeadGrass.java
@@ -3,11 +3,28 @@ package foxforgebiomes.blocks;
 import foxforgebiomes.api.BasicBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+
+import java.util.Random;
 
 public class BlockDeadGrass extends BasicBlock {
     public BlockDeadGrass() {
         super("dead_grass", Material.GRASS, true);
-        setSoundType(SoundType.PLANT);
-
+        this.setSoundType(SoundType.PLANT);
+        this.setHarvestLevel("shovel", 0);
+        this.setHardness((float) .5);
     }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Item.getItemFromBlock(Blocks.DIRT);
+    }
+
+    @Override
+    public int quantityDropped(Random random) {
+        return 1;
+    }
+    
 }
