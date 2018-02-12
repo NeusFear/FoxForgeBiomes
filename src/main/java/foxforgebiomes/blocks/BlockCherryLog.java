9c016ecc3280453d2d7c4e9a3ce128f9eea2ea87
Foxwar5 <30801725+Foxwar5@users.noreply.github.com>
2018-02-12 17:21:43 -0600

    added cherry log BOIIIII
    litty kitty

---
+++ src/main/java/foxforgebiomes/blocks/BlockCherryLog.java
@@ -0,0 +1,9 @@
+package foxforgebiomes.blocks;
+
+import foxforgebiomes.api.BasicLog;
+
+public class BlockCherryLog extends BasicLog {
+    public BlockCherryLog() {
+        super("cherry");
+    }
+}
